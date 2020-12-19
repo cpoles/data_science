23ea9c3c4899bb221f59760cedd1f52a96bdbef7
cpoles <cadupoles@gmail.com>
2020-12-19 00:23:13 -0300

    Added basic bot auto message functionality

--- whatspp_bot/my_wa_bot.py
+++ whatspp_bot/my_wa_bot.py
@@ -3,7 +3,35 @@
 
 #import necessary libraries
 from whatsapp_api import  WhatsApp
+from time import sleep
+
 
 # create the WhatsApp object
 wp = WhatsApp()
+
+#workaround to wait the user scanning the QR Code
+input("Pressione Enter after scanning QR Code")
+
+contact_names = ['Ge Bot', 'Diego Bot', 'Nedo Bot']
+
+first_names = [n.split(' ')[0] for n in contact_names]
+
+product_list = ['Lettuce', 'Beans', 'Carrots']
+
+for first_name, full_name, product in zip (first_names, contact_names, product_list):
+    #search contact
+    wp.search_contact(full_name)
+    sleep(3)
+    
+    #create message
+    msg = "Hi, {first_name}. Thanks for buying {product}!"
+    
+    #send message
+    wp.send_message(msg)
+
+
+#wait 10 seconds befor closing Chrome
+sleep(10)
+wp.driver.close()
+
 
