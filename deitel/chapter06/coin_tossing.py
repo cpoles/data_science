53a0d63fdb7c61db5da9117a3307975bb19d2378
cpoles <cadupoles@gmail.com>
2021-01-15 14:14:29 -0300

    Added coin tossing

---
+++ deitel/chapter06/coin_tossing.py
@@ -0,0 +1,10 @@
+# coin_tossing.py
+'''Dynamically graphing frequencies of coin tossings'''
+
+#import libraries
+from matplotlib import animation
+import matplotlib.pyplot as plt 
+import random
+import seaborn as sns
+import sys
+
