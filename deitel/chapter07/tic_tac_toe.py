9d8d4e9a2c68da5eee98a468b138711d78d9902c
cpoles <cadupoles@gmail.com>
2021-01-18 17:21:52 -0300

    Finished Tic Tac Toe Game

--- deitel/chapter07/tic_tac_toe.py
+++ deitel/chapter07/tic_tac_toe.py
@@ -4,12 +4,33 @@
 # import libraries
 import numpy as np
 
-board = np.full((3, 3), 'T')
+
+def check_input(char):
+    '''Checks user's input's validity'''
+    if len(char) == 1 and char.isdigit():
+        dig = int(char)
+        if dig > 2:
+            raise ValueError('The board is 3x3. Only numbers 0-2 allowed.')
+    else:
+        raise ValueError('Only positive numbers allowed')
+
+    return dig
+
+
+def write_board(row, column, board, letter):
+    '''Writes user input to the board'''
+    if board[row, column] == ' ':
+        board[row, column] = letter
+        print(f'Entered {letter} at position ({row}, {column})')
+        return True
+
+    print('Wrong position. Try again...')
+    return False
 
 
 def check_seq(seq):
     '''Checks if all elems of a sequence are equal'''
-    return len(set(seq)) == 1
+    return len(set(seq)) == 1 if ' ' not in seq else False
 
 
 def get_diagonals(multidim):
@@ -23,10 +44,72 @@ def get_diagonals(multidim):
 def check_win(multidim):
     '''Checks for a winning combination'''
     # check rows
-    rows = all(list(map(check_seq, multidim)))
+    rows = any(list(map(check_seq, multidim)))
     # check columns
-    columns = all(list(map(check_seq, multidim.T)))
+    columns = any(list(map(check_seq, multidim.T)))
     # check diagonals
     diagonals = any(list(map(check_seq, get_diagonals(multidim))))
 
     return rows or columns or diagonals
+
+
+# Define a print board method
+def print_board(board):
+    '''Prints the current board's state'''
+    board_str = ''
+    for row in board:
+        board_str += '|'
+        for column in row:
+            board_str += f' {column} |'
+        print(board_str)
+        print('------------')
+        board_str = ''
+
+
+def game():
+    '''Simulates the tic tac toe game'''
+    p1_count = 0
+    p2_count = 0
+    board = np.full((3, 3), ' ')  # fills the board with empty spaces
+    p1_turn = False
+    # p2_turn = False
+    letter = ''
+    print_board(board)  # print initial board
+
+    while True:
+        # get players turn
+        if not p1_turn:
+            p1_turn = True
+            p1_count += 1
+            letter = 'X'
+            print(f"Player's 1 turn. Letter: {letter}. Round {p1_count}.")
+        else:
+            p1_turn = False
+            # p2_turn = True
+            p2_count += 1
+            letter = 'O'
+            print(f"Player's 2 turn. Letter: {letter}. Round {p2_count}.")
+
+        # get and check user input
+        p_input = input('Enter row number: ')
+        row = check_input(p_input)
+        p_input = input('Enter column number: ')
+        column = check_input(p_input)
+
+        # write to the board
+        if write_board(row, column, board, letter):
+            print_board(board)
+            if check_win(board):
+                if p1_turn:
+                    print('Player 1 Wins!')
+                else:
+                    print('Player 2 Wins!')
+
+                print_board(board)
+                break
+        else:
+            p1_turn = not p1_turn
+
+
+# play the game
+game()
