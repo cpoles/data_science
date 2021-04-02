902419dd9da0fff4e4166c1ab29983454a8c9fde
cpoles <cadupoles@gmail.com>
2021-04-01 23:46:17 -0300

    Added helpers file and mammoscan class file

--- projects/machine_learning/deep_learning/mias_mammography/.ipynb_checkpoints/helpers-checkpoint.py
+++ projects/machine_learning/deep_learning/mias_mammography/.ipynb_checkpoints/helpers-checkpoint.py
@@ -1,108 +1,38 @@
-# MammoScan class 
-
-class MammoScan:
-    def __init__(self, scan, sc_info):
-        self.scan = scan
-        self.sc_info = sc_info
-
-    @property
-    def get_x(self):
-        return self.sc_info.x
-    
-    @property
-    def get_y(self):
-        return self.sc_info.y
-        
-    @property
-    def get_radius(self):
-        return self.sc_info.radius
-    
-    @property
-    def get_scan(self):
-        return self.scan
-
-    @property
-    def get_class(self):
-        return self.sc_info['class']
-
-    @property
-    def transformations(self):
-        return self.__transform
-
-    @property
-    def plot(self):
-        img = self.get_scan
-
-        # Create a figure. Equal aspect so circles look circular
-        fig, ax = plt.subplots(1)
-
-        fig.set_size_inches(12, 10)
-        ax.set_aspect('equal')
-
-        # Show the image
-        ax.imshow(img)
-        ax.set_ylim(bottom=0, top=1024)
-
-        # create a circle to patch on the image
-        x = pd.to_numeric(self.get_x)
-        y = pd.to_numeric(self.get_y)
-        r = pd.to_numeric(self.get_radius)
-        circ = Circle((x,1024-y), r, fill=False)
-        ax.add_patch(circ)
-        print(x, y, r)
-    
-    # private method
-    def _set_x(self, xValue):
-        self.sc_info.loc['x'] = xValue
-    
-    # private method
-    def _set_y(self, yValue):
-        self.sc_info.y = yValue
-    
-    # private method
-    def _set_radius(self, rValue):
-        self.sc_info.radius = rValue
-        
-    # private method
-    def __get_crop_coords(self):
-        '''Returns a tuple with x, y and r'''
-        # check scan class to decide on how to crop
-        if pd.isnull(self.get_radius):
-            self._set_radius(48.0)
-        if pd.isnull(self.get_x):
-            x = float(np.random.randint(500, 513))
-            self._set_x(x)
-        if pd.isnull(self.get_y):
-            y = float(np.random.randint(500, 513))
-            self._set_y(y)
-            
-        return (self.get_x, self.get_y, self.get_radius)
-    
-    # private method
-    def __transform(self):
-        '''Creates a dict 
-                  with rotated and mirrored versions of self.scan'''
-        # create dictionary
-        transformations = dict()
-        # get crop values
-        x, y, r = self.__get_crop_coords()
-        # crop and resize scan
-        cropped_scan = self.get_scan.crop((x-r, y-r, x+r, y+r))
-        resized_scan = cropped_scan.resize((48,48))
-        # create rotated images
-        for angle in [0, 90, 180, 270]:
-            rotated = resized_scan.rotate(angle) # rotated by angle
-            mirr_lr = rotated.transpose(Image.FLIP_LEFT_RIGHT)
-            mirr_tp = rotated.transpose(Image.FLIP_TOP_BOTTOM)
-            transformations[angle] = dict(zip(['rotated', 'mirr_lr', 'mirr_tp'], 
-                                         [rotated, mirr_lr, mirr_tp]))
-
-        return transformations
-
-
 # helper functions
 
-def create_img_dic(path: str) -> dict:
+def clean_ds_files(df: pd.DataFrame) -> pd.DataFrame:
+    new_df = df.copy()
+    # search for invalid x values for removal
+    indices = new_df.x[lambda x: x == '*NOTE'].index
+    
+    for idx in indices:
+        n_idx = new_df.index.get_loc(idx)
+        # drop from dataset
+        new_df.drop(new_df.index[n_idx], inplace=True)
+        # delete from directory
+        delete_image(idx)
+
+    # make x and y float values
+    new_df.x = new_df.x.astype(float)
+    new_df.y = new_df.y.astype(float)
+    
+    return new_df
+
+
+def delete_image(filename: str, directory='./all-mias/'):
+    '''Deletes original image files that won't be initially used'''
+    paths = Path(directory).glob('**/*.pgm')
+    filename += '.pgm'
+    for f_path in sorted(paths):
+        try:
+            if f_path.name == filename:
+                os.remove(f_path)
+                break
+        except FileNotFoundError as fnf:
+            print('{fnf}') 
+            
+            
+def create_scan_filenames_dic(path: str) -> dict:
     '''Creates a dictionary with image filenames'''
     paths = Path(path).glob('**/*.pgm')
     img_dic = dict()
@@ -116,32 +46,82 @@ def create_img_dic(path: str) -> dict:
     
     return img_dic
 
-def get_img_data(filename: str) -> pd.Series:
-    '''Returns Series with scan information'''
+
+def get_scan_info(filename: str) -> pd.Series:
     try:
-        img_data = test_df.loc[filename]
-        #print(img_data)
-        return img_data
+        scan_info = test_df.loc[filename]
+        return scan_info
     except KeyError as ie:
         print('Invalid Index')
         
 
-def create_filename(scan_name: str, transf_dic: dict) -> list:
+def save_subsamples(scans_dic: dict(), df: pd.DataFrame) -> pd.DataFrame:
+    # define subsamples folder
+    folder = 'subsamples'
+    df_sub = pd.DataFrame()
+    try:
+        # create if not yet
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+    except:
+        print('An error occurred when searching for the folder')
+        
+    # iterate dictionary of filenames
+    for scan_name, filename in scans_dic.items():
+        
+        # create image and scan info objects
+        try:
+            scan = Image.open(filename)
+        except FileNotFoundError as fnf:
+            print({fnf})
+            
+        scan_info = df.loc[scan_name].copy()
+        # create the MammoScan object
+        m_scan = MammoScan(scan, scan_info)
+        # get the transformations
+        transf_scans = m_scan.transformations
+        # create filenames
+        filenames = create_subsample_filename(scan_name, transf_scans)
+        # get transformed scans Image objects
+        imgs = get_transformed_scans(transf_scans)
+        # prepare for saving
+        fs_and_is = list(zip(filenames, imgs))
+        
+        for filename, image in fs_and_is:
+            # create new observation with subsample name
+            scan_info.name = filename
+            # append to dataframe
+            df_sub = df_sub.append(scan_info)
+            print(scan_name)
+            path = os.path.join('./subsamples', filename) 
+            print(path)
+            try:
+                pass
+                #image.save(path, compress_level=0)
+            except ValueError as ve:
+                print('Output format could not be determined from the file name.')
+            except OSError as ose:
+                print('File could not be written.')
+                print({ose})
+        
+    return df_sub
+
+
+def create_subsample_filename(scan_name: str, transf_dic: dict) -> list:
     '''Creates suffix pattern filename for transformed scans'''
     filename = ''
     file_names = list()
     for angle, transfs in transf_dic.items():
         for tf in transfs.keys():
-            filename += f'{scan_name}_{angle}_{tf}.pgm'
+            filename += f'{scan_name}_{angle}_{tf}.png'
             #print(filename)
             file_names.append(filename)
             filename = ''
-     
+            
     return file_names
 
 
 def get_transformed_scans(transf_dic: dict):
-    '''Returns a list of transformed image scans'''
     scans = list()
     for angle, transfs in transf_dic.items():
         for scan in transfs.values():
@@ -149,32 +129,24 @@ def get_transformed_scans(transf_dic: dict):
     
     return scans
 
-def save_subsamples(scans_dic: dict(), df: pd.DataFrame):
-    '''Save image subsample to the subsamples folder'''
-    # define subsamples folder
-    folder = 'subsamples'
-    # create if not yet
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-    
-    # iterate dictionary of filenames
-    for scan_name, filename in scans_dic.items():
-        
-        # create image and scan info objects
-        scan = Image.open(filename)
-        scan_info = df.loc[scan_name].copy()
-        # create the MammoScan object
-        m_scan = MammoScan(scan, scan_info)
-        # get the transformations
-        transf_scans = m_scan.transformations()
-        # create filenames
-        filenames = create_filename(scan_name, transf_scans)
-        # get transformed scans Image objects
-        imgs = get_transformed_scans(transf_scans)
-        # prepare for saving
-        fs_and_is = list(zip(filenames, imgs))
-        for filename, image in fs_and_is:
-            print(scan_name)
-            path = './subsamples/' + filename
-            print(path)
-            image.save(path)
+
+def plot_scan(scan: MammoScan):
+    img = scan.scan
+
+    # Create a figure. Equal aspect so circles look circular
+    fig, ax = plt.subplots(1)
+
+    fig.set_size_inches(12, 10)
+    ax.set_aspect('equal')
+
+    # Show the image
+    ax.imshow(img)
+    ax.set_ylim(bottom=0, top=1024)
+
+    # create a circle to patch on the image
+    x = pd.to_numeric(scan.x)
+    y = pd.to_numeric(scan.y)
+    r = pd.to_numeric(scan.radius)
+    circ = Circle((x,1024-y), r, fill=False)
+    ax.add_patch(circ)
+    print(x, y, r)
---
+++ projects/machine_learning/deep_learning/mias_mammography/mammoscan.py
@@ -0,0 +1,110 @@
+class MammoScan:
+    def __init__(self, scan, sc_info):
+        self._scan = scan
+        self._sc_info = sc_info
+    
+    @property
+    def scan(self):
+        return self._scan
+    
+    @property
+    def scan_info(self):
+        return self._sc_info
+    
+    @property
+    def x(self):
+        return self._sc_info.x
+    
+    @property
+    def y(self):
+        return self._sc_info.y
+    
+    @property
+    def radius(self):
+        return self._sc_info.radius
+
+    @property
+    def ab_class(self):
+        return self._sc_info.ab_class
+    
+    @property
+    def bg(self):
+        return self._sc_info.bg
+    
+    @property
+    def severity(self):
+        return self._sc_info.severity
+
+    @property
+    def transformations(self):
+        return self.__transform()
+    
+    # instance method
+    def plot(self):
+        img = self.scan
+
+        # Create a figure. Equal aspect so circles look circular
+        fig, ax = plt.subplots(1)
+
+        fig.set_size_inches(12, 10)
+        ax.set_aspect('equal')
+
+        # Show the image
+        ax.imshow(img)
+        ax.set_ylim(bottom=0, top=1024)
+
+        # create a circle to patch on the image
+        x = pd.to_numeric(self.x)
+        y = pd.to_numeric(self.y)
+        r = pd.to_numeric(self.radius)
+        circ = Circle((x,1024-y), r, fill=False)
+        ax.add_patch(circ)
+        print(x, y, r)
+    
+    # private method
+    def __set_x(self, xValue):
+        self._sc_info.x = xValue
+    
+    # private method
+    def __set_y(self, yValue):
+        self._sc_info.y = yValue
+    
+    # private method
+    def __set_radius(self, rValue):
+        self._sc_info.radius = rValue
+        
+    # private method
+    def __get_crop_coords(self):
+        '''Returns a tuple with x, y and r'''
+        # check scan class to decide on how to crop
+        if pd.isnull(self.radius):
+            self.__set_radius(48.0)
+        if pd.isnull(self.x):
+            x = float(np.random.randint(500, 513))
+            self.__set_x(x)
+        if pd.isnull(self.y):
+            y = float(np.random.randint(500, 513))
+            self.__set_y(y)
+            
+        return (self.x, self.y, self.radius)
+    
+    # private method
+    def __transform(self):
+        '''Creates a dict 
+                  with rotated and mirrored versions of self.scan'''
+        # create dictionary
+        transformations = dict()
+        # get crop values
+        x, y, r = self.__get_crop_coords()
+        # crop and resize scan
+        cropped_scan = self.scan.crop((x-r, y-r, x+r, y+r))
+        resized_scan = cropped_scan.resize((48,48))
+        # create rotated images
+        for angle in [0, 90, 180, 270]:
+            rotated = resized_scan.rotate(angle) # rotated by angle
+            mirr_lr = rotated.transpose(Image.FLIP_LEFT_RIGHT)
+            mirr_tp = rotated.transpose(Image.FLIP_TOP_BOTTOM)
+            transformations[angle] = dict(zip(['rotated', 'mirr_lr', 'mirr_tp'], 
+                                         [rotated, mirr_lr, mirr_tp]))
+
+        return transformations
