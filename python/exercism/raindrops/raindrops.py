9ada8f05a06199b013371c7c8cc483b634a0aaa4
cpoles <cadupoles@gmail.com>
2021-08-19 20:24:30 -0300

    passed raindrops

--- python/exercism/raindrops/raindrops.py
+++ python/exercism/raindrops/raindrops.py
@@ -1,2 +1,14 @@
 def convert(number):
-    pass
+    raindrops = str()
+
+    if number % 3 == 0:
+        raindrops += "Pling"
+    if number % 5 == 0:
+        raindrops += "Plang"
+    if number % 7 == 0:
+        raindrops += "Plong"
+
+    if len(raindrops) == 0:
+        return str(number)
+    else:
+        return raindrops
