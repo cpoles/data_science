3db70a9236ccc3a04b25e93930fadd92eb8ac6a5
cpoles <cadupoles@gmail.com>
2021-08-19 22:05:14 -0300

    passed matrix

--- python/exercism/matrix/matrix.py
+++ python/exercism/matrix/matrix.py
@@ -1,9 +1,17 @@
 class Matrix:
+
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
+        self.matrix = [*map(lambda x: x.split(),
+                            self.matrix_string.split(sep='\n'))]
 
     def row(self, index):
-        pass
+        if (index < 1) or (index > len(self.matrix)):
+            raise Exception("Invalid Index.")
+        return [*map(int, self.matrix[index-1])]
 
     def column(self, index):
-        pass
+        if (index < 1) or (index > len(self.matrix[0])):
+            raise Exception("Invalid Index.")
+        return [int(self.matrix[x][index-1]) for x in range(len(self.matrix))]
+
