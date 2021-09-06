ee4a88c6d33215d5b6147be3427afb6f02eab104
cpoles <cadupoles@gmail.com>
2021-09-06 14:52:56 -0300

    Finished regressions

---
+++ machine_learning/udacity/outliers/.ipynb_checkpoints/enron_outliers-checkpoint.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import joblib
+import sys
+import matplotlib.pyplot
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+
+### read in data dictionary, convert to numpy array
+data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb") )
+features = ["salary", "bonus"]
+data = featureFormat(data_dict, features)
+
+
+### your code below
+
+
+
