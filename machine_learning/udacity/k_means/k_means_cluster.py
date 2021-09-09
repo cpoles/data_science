847cb339a0b76d81d7a4e8449585f4b1bdd7cfea
cpoles <cadupoles@gmail.com>
2021-09-09 15:25:43 -0300

    finished feature scaling

--- machine_learning/udacity/k_means/k_means_cluster.py
+++ machine_learning/udacity/k_means/k_means_cluster.py
@@ -6,13 +6,12 @@
 
 
 import joblib
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
-
+from sklearn.preprocessing import MinMaxScaler
 
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
@@ -52,6 +51,27 @@ features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+#rescaling salary and exercised stock options
+scaler = MinMaxScaler()
+
+finance_features = np.array(finance_features)
+
+min_salary = finance_features[:, 0].min()
+max_salary = finance_features[:, 0].max()
+
+min_st = finance_features[:, 1].min()
+max_st = finance_features[:, 1].max()
+
+
+print(max_salary)
+
+# salary 200_000 and exercised stock option 1_000_000
+sal = (200_000 - min_salary) / (max_salary - min_salary)
+st = (1_000_000 - min_st) / (max_st - min_st)
+print(sal)
+print(st)
+
+finance_features = scaler.fit_transform(finance_features)
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
