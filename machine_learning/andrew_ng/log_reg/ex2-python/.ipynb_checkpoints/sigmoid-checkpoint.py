d5bac25cdde811fcc3f35c82249d2dd9f51afa6c
cpoles <cadupoles@gmail.com>
2021-09-29 09:14:11 -0300

    finished ex2

--- machine_learning/andrew_ng/log_reg/ex2-python/.ipynb_checkpoints/sigmoid-checkpoint.py
+++ machine_learning/andrew_ng/log_reg/ex2-python/.ipynb_checkpoints/sigmoid-checkpoint.py
@@ -1,1 +1,11 @@
+# sigmoid.py
+
+import numpy as np
 # define sigmoid function
+
+def sigmoid(x):
+    try:
+        sig = 1 / (1 + np.exp(-x))
+    except:
+        raise ValueError('Error')
+    return sig
