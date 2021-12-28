4d89e1360ec587567b880d41e8045565d25b0e23
cpoles <cadupoles@gmail.com>
2021-12-28 14:41:41 -0300

    added card_images folder

--- machine_learning/deitel/oop/deck.py
+++ machine_learning/deitel/oop/deck.py
@@ -8,9 +8,34 @@ class DeckOfCards:
 
     def __init__(self) -> None:
         '''Initialize the deck'''
-        self._current_card = 0
+        self._current_card = 0 # idx of current card in self._deck
         self._deck = []
 
         for count in range(DeckOfCards.NUMBER_OF_CARDS):
             self._deck.append(Card(Card.FACES[count % 13], 
                                     Card.SUITS[count // 13])) 
+
+    def shuffle(self):
+        '''Shuffle deck.'''
+        self._current_card = 0
+        random.shuffle(self._deck)
+
+    def deal_card(self):
+        '''Return one Card'''
+        try:
+            card = self._deck[self._current_card]
+            self._current_card += 1
+            return card
+        except:
+            return None
+
+    def __str__(self) -> str:
+        '''Return a string representation of the current deck'''
+        s = ''
+        
+        for index, card in enumerate(self._deck):
+            s += f'{self._deck[index]:<19}'
+            if (index + 1) % 4 == 0:
+                s += '\n'
+        
+        return s
