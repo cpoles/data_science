063c9b2bab1ceb9d38bc66cf1d724a3d6eb13cbe
cpoles <cadupoles@gmail.com>
2021-12-29 12:49:11 -0300

    finihsed carddataclass

--- machine_learning/deitel/oop/carddataclass.py
+++ machine_learning/deitel/oop/carddataclass.py
@@ -9,3 +9,25 @@ from typing import ClassVar, List
 @dataclass
 class Card:
     
+    # class attributes
+    FACES: ClassVar[List[str]] = ['Ace', '2', '3', '4', '5', '6', '7',
+                                    '8', '9', '10', 'Jack', 'Queen', 'King']
+    
+    SUITS: ClassVar[List[str]] = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+    # data attributes
+    face: str
+    suit: str
+
+    @property
+    def image_name(self):
+        '''Return the Card's image file name.'''
+        return str(self).lower().replace(' ', '_') + '.png'
+
+    def __str__(self) -> str:
+        '''Return string represenattion for str()'''
+        return f'{self.face} of {self.suit}'
+    
+    def __format__(self, format: str) -> str:
+        '''Return formatted string representation.'''
+        return f'{str(self):{format}}'
