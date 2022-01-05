38504600da808d0770cb087ce1f193997ee02d6d
cpoles <cadupoles@gmail.com>
2022-01-05 19:07:13 -0300

    finished raindrops

--- python/exercism/python/raindrops/raindrops.py
+++ python/exercism/python/raindrops/raindrops.py
@@ -1,2 +1,15 @@
 def convert(number):
-    pass
+    raindrops = {3: 'Pling', 5: 'Plang', 7:'Plong'}
+    sound = ''
+
+    for key in raindrops:
+        if number % key == 0:
+            sound += raindrops[key]
+    
+    if sound == '':
+        return str(number)
+
+    return sound
+
+
+    
