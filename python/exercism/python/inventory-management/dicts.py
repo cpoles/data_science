ed8c0a9bc5f5ce11df33551403a453712915ef37
cpoles <cadupoles@gmail.com>
2022-01-05 12:59:08 -0300

    added inventory-management

---
+++ python/exercism/python/inventory-management/dicts.py
@@ -0,0 +1,50 @@
+def create_inventory(items):
+    """
+
+    :param items: list - list of items to create an inventory from.
+    :return:  dict - the inventory dictionary.
+    """
+
+    pass
+
+
+def add_items(inventory, items):
+    """
+
+    :param inventory: dict - dictionary of existing inventory.
+    :param items: list - list of items to update the inventory with.
+    :return:  dict - the inventory dictionary update with the new items.
+    """
+
+    pass
+
+
+def decrement_items(inventory, items):
+    """
+
+    :param inventory: dict - inventory dictionary.
+    :param items: list - list of items to decrement from the inventory.
+    :return:  dict - updated inventory dictionary with items decremented.
+    """
+
+    pass
+
+
+def remove_item(inventory, item):
+    """
+    :param inventory: dict - inventory dictionary.
+    :param item: str - item to remove from the inventory.
+    :return:  dict - updated inventory dictionary with item removed.
+    """
+
+    pass
+
+
+def list_inventory(inventory):
+    """
+
+    :param inventory: dict - an inventory dictionary.
+    :return: list of tuples - list of key, value pairs from the inventory dictionary.
+    """
+
+    pass
