6371ceb9915bee9d43ce7f35bd971415aee554ca
cpoles <cadupoles@gmail.com>
2022-01-06 01:14:42 -0300

    finished hamming

--- python/exercism/python/hamming/hamming.py
+++ python/exercism/python/hamming/hamming.py
@@ -1,2 +1,6 @@
 def distance(strand_a, strand_b):
-    pass
+    if not len(strand_a) == len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    return sum([a != b for a, b in zip(strand_a, strand_b)])
+    
