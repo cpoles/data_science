10f343b8d1c36a9f7c6e2e9847339d4d7e204964
cpoles <cadupoles@gmail.com>
2022-01-06 01:03:36 -0300

    finished matrix

--- python/exercism/python/matrix/matrix.py
+++ python/exercism/python/matrix/matrix.py
@@ -1,9 +1,12 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string        
 
     def row(self, index):
-        pass
+        rows = self.matrix_string.split('\n')
+        return [int(x) for x in rows[index - 1].split()]
 
     def column(self, index):
-        pass
+        rows = self.matrix_string.split('\n')
+        rows = [[int(x) for x in row.split()] for row in rows]
+        return [row[index -1] for row in rows]
