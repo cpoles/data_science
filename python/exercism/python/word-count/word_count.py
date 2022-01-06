4eee09862329400a12a4d4127e257102bec35081
cpoles <cadupoles@gmail.com>
2022-01-06 14:18:01 -0300

    finished word_count

--- python/exercism/python/word-count/word_count.py
+++ python/exercism/python/word-count/word_count.py
@@ -1,2 +1,8 @@
+import re
+from collections import Counter
+
 def count_words(sentence):
-    pass
+    words = re.findall(r"[a-z0-9]+(?:'[a-z]+)?", sentence.lower())
+    return Counter(words)
+    
+
