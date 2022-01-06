8a291c3ba36574a9081e4ce913467be87fa104ce
cpoles <cadupoles@gmail.com>
2022-01-06 01:52:08 -0300

    finished isogram

--- python/exercism/python/isogram/isogram.py
+++ python/exercism/python/isogram/isogram.py
@@ -1,2 +1,6 @@
 def is_isogram(string):
-    pass
+    string = string.lower()
+    # remove space and hyphens
+    string = [char for char in string if char not in [' ', '-']]
+
+    return len(string) == len(set(string))
