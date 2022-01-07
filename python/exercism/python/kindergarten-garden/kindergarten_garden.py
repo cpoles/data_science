bbb54583696dc367502a870d7dd8c981031b1687
cpoles <cadupoles@gmail.com>
2022-01-07 00:08:48 -0300

    finished kindergarten-garden

--- python/exercism/python/kindergarten-garden/kindergarten_garden.py
+++ python/exercism/python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,24 @@
+STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
+		    'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+
+PLANTS = {
+    'C': 'Clover',
+    'G': 'Grass',
+    'V': 'Violets',
+    'R': 'Radishes'
+}
+
 class Garden:
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram, students=STUDENTS):
+        self.diagram = diagram.split('\n')
+        self.students = sorted(students)
+
+    def plants(self, student):
+        idx = self.students.index(student)
+
+        plants_lst = []
+
+        for row in self.diagram:
+            plants_lst += list(map(lambda x: PLANTS[x], row[idx*2:idx*2+2]))
+
+        return plants_lst
