88e2fde961c8830961220f56784fd8aa7573666f
cpoles <cadupoles@gmail.com>
2022-01-10 15:44:20 -0300

    finished black-jack

--- python/exercism/python/black-jack/black_jack.py
+++ python/exercism/python/black-jack/black_jack.py
@@ -3,7 +3,14 @@
 How to play blackjack:    https://bicyclecards.com/how-to-play/blackjack/
 "Standard" playing cards: https://en.wikipedia.org/wiki/Standard_52-card_deck
 """
+DECK = {
+    '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+    '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K':10, 'A': 1
+}
 
+TENS = {
+    '10': 10, 'J': 10, 'Q': 10, 'K':10, 
+}
 
 def value_of_card(card):
     """Determine the scoring value of a card.
@@ -12,7 +19,8 @@ def value_of_card(card):
     :return: int - value of a given card (J, Q, K = 10, 'A' = 1) numerical value otherwise.
     """
 
-    pass
+    return DECK[card]
+
 
 def higher_card(card_one, card_two):
     """Determine which card has a higher value in the hand.
@@ -20,8 +28,10 @@ def higher_card(card_one, card_two):
     :param card_one, card_two: str - cards dealt.  J, Q, K = 10, 'A' = 1, all others are numerical value.
     :return: higher value card - str. Tuple of both cards if they are of equal value.
     """
+    value_co = DECK[card_one]
+    value_ct = DECK[card_two]
 
-    pass
+    return (card_one, card_two) if value_co == value_ct else (card_one if value_co > value_ct else card_two)
 
 
 def value_of_ace(card_one, card_two):
@@ -30,8 +40,7 @@ def value_of_ace(card_one, card_two):
     :param card_one, card_two: str - card (J, Q, K == 10, numerical value otherwise)
     :return: int - value of the upcoming ace card (either 1 or 11).
     """
-
-    pass
+    return 11 if DECK[card_one] + DECK[card_two] < 11 else 1
 
 
 def is_blackjack(card_one, card_two):
@@ -41,7 +50,8 @@ def is_blackjack(card_one, card_two):
     :return: bool - if the hand is a blackjack (two cards worth 21).
     """
 
-    pass
+    return True if (card_one == 'A' and card_two in TENS) or \
+                   (card_two == 'A' and card_one in TENS) else False 
 
 
 def can_split_pairs(card_one, card_two):
@@ -51,7 +61,7 @@ def can_split_pairs(card_one, card_two):
     :return: bool - if the hand can be split into two pairs (i.e. cards are of the same value).
     """
 
-    pass
+    return True if DECK[card_one] == DECK[card_two] else False
 
 
 def can_double_down(card_one, card_two):
@@ -61,4 +71,4 @@ def can_double_down(card_one, card_two):
     :return: bool - if the hand can be doubled down (i.e. totals 9, 10 or 11 points).
     """
 
-    pass
+    return True if DECK[card_one] + DECK[card_two] in (9, 10, 11) else False 
