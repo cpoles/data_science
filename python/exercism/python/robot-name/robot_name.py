404f2fc0f866a1be20aeea6797279525b1f41699
cpoles <cadupoles@gmail.com>
2022-01-10 23:34:42 -0300

    finished  robot-name

--- python/exercism/python/robot-name/robot_name.py
+++ python/exercism/python/robot-name/robot_name.py
@@ -1,3 +1,23 @@
+import string
+import random
+
 class Robot:
     def __init__(self):
-        pass
+        self.name = self._create_name()
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = self._create_name()
+
+    def _create_name(self):
+        letters = ''.join(random.choices(string.ascii_uppercase, k=2))
+        numbers = ''.join(random.choices(string.digits, k=3))
+        return letters + numbers
+
+    def reset(self):
+        random.seed(random.randint(0, 100))
+        self.name = self._create_name()
