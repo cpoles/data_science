c6c2246c987a603dc1f7e24251412f34b059abe7
cpoles <cadupoles@gmail.com>
2022-01-14 09:08:55 -0300

    finished protein-translation

--- python/exercism/python/protein-translation/protein_translation.py
+++ python/exercism/python/protein-translation/protein_translation.py
@@ -1,2 +1,31 @@
+CODONS_PROTEIN = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP"
+
+}
+
+import re
+
 def proteins(strand):
-    pass
+    sequence = []
+    codons = re.findall(r'[ACUG]{3}', strand)
+    for codon in codons:
+        if CODONS_PROTEIN[codon] == 'STOP':
+            break
+        sequence.append(CODONS_PROTEIN[codon])
+    return sequence
