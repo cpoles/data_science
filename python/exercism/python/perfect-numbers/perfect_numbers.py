4f8404e115d16c863d19dba31aa90a572ddeeca3
cpoles <cadupoles@gmail.com>
2022-01-19 20:28:00 -0300

    finished perfect-numbers

--- python/exercism/python/perfect-numbers/perfect_numbers.py
+++ python/exercism/python/perfect-numbers/perfect_numbers.py
@@ -1,7 +1,18 @@
+import math
 def classify(number):
     """ A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number < 1:
+        raise ValueError("Classification is only possible for positive integers.")
+
+    factors = [divisor for divisor in list(range(1, number // 2 + 1)) if number % divisor == 0]
+
+    if sum(factors) == number:
+        return 'perfect'
+    elif sum(factors) > number:
+        return 'abundant'
+    else:
+        return 'deficient'
