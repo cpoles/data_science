5150a2ecf0e932c059cb2256f34cce642384a0b6
cpoles <cadupoles@gmail.com>
2022-01-20 15:18:31 -0300

    finished anagram

--- python/exercism/python/anagram/anagram.py
+++ python/exercism/python/anagram/anagram.py
@@ -1,2 +1,7 @@
 def find_anagrams(word, candidates):
-    pass
+    return [candidate for candidate in candidates if is_anagram(word, candidate)]
+
+def is_anagram(word, candidate):
+    w = word.lower()
+    c = candidate.lower()
+    return w != c and sorted(w) == sorted(c)
