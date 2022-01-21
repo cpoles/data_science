ea421cc85dc506b2d475ecccafcf4aca898da849
cpoles <cadupoles@gmail.com>
2022-01-21 13:31:12 -0300

    finished series

--- python/exercism/python/series/series.py
+++ python/exercism/python/series/series.py
@@ -1,2 +1,15 @@
 def slices(series, length):
-    pass
+    validate_slices(series, length)
+
+    return [series[i:i+length] for i in range(0, len(series) - length + 1)]    
+
+
+def validate_slices(series, slice):
+    if slice == 0:
+        raise ValueError("slice length cannot be zero")
+    if slice < 0:
+        raise ValueError("slice length cannot be negative")
+    if not series:
+        raise ValueError("series cannot be empty")
+    if slice > len(series):
+        raise ValueError("slice length cannot be greater than series length")
