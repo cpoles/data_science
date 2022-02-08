3bef9a8079784b94e5666907902df1c13e46e447
cpoles <cadupoles@gmail.com>
2022-02-07 22:31:33 -0300

    finished prime-factors

--- python/exercism/python/prime-factors/prime_factors.py
+++ python/exercism/python/prime-factors/prime_factors.py
@@ -1,2 +1,13 @@
 def factors(value):
-    pass
+    primes = []
+    prime = 2
+    while True:
+     if value == 1:
+         break
+     if value % prime == 0:
+         value = value // prime
+         primes.append(prime)
+     else:
+         prime = prime + 1
+
+    return primes
