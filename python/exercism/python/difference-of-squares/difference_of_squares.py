23aa38fd178636d423ebb131a3ad5115707e9dc5
cpoles <cadupoles@gmail.com>
2022-02-22 21:13:45 -0300

    finished difference-of-squares

--- python/exercism/python/difference-of-squares/difference_of_squares.py
+++ python/exercism/python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,10 @@
 def square_of_sum(number):
-    pass
+    return sum(list(range(1, number + 1))) ** 2
 
 
 def sum_of_squares(number):
-    pass
+    return sum([x ** 2 for x in range(1, number + 1)])
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
