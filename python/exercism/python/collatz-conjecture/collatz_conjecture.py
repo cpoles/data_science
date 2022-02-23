892a749f3ff56bb4b6a5c00a7f6954409467eeed
cpoles <cadupoles@gmail.com>
2022-02-22 21:06:19 -0300

    finished collatz-conjecture

--- python/exercism/python/collatz-conjecture/collatz_conjecture.py
+++ python/exercism/python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,15 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError('Only positive integers are allowed')
+    n_steps = 0
+    while True:
+        if number == 1:
+            break
+
+        if number % 2 == 0:
+            number /= 2
+        else:
+            number = 3 * number + 1
+
+        n_steps += 1
+    return n_steps
