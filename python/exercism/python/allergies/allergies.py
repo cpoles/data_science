38628f267109623b108e98cafac7d41ee994c5d9
cpoles <cadupoles@gmail.com>
2022-03-09 01:10:36 -0300

    initial class

--- python/exercism/python/allergies/allergies.py
+++ python/exercism/python/allergies/allergies.py
@@ -1,11 +1,25 @@
 class Allergies:
 
     def __init__(self, score):
-        pass
+        self.score = score
 
     def allergic_to(self, item):
-        pass
+        return True if item in self.lst else False
 
     @property
     def lst(self):
-        pass
+        items = {
+            1: "eggs",
+            2: "peanuts",
+            4: "shellfish",
+            8: "strawberries",
+            16: "tomatoes",
+            32: "chocolate",
+            64: "pollen",
+            128: "cats"
+        }
+        if self.score == 0:
+            allergies = []
+        else:
+            allergies = [allergy for key, allergy in items.items() if key <= (self.score % 128)]
+        return allergies
