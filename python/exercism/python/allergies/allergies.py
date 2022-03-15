f638633fa5b180fefda12918e7be436de46acdd7
cpoles <cadupoles@gmail.com>
2022-03-14 23:34:37 -0300

    finished allergies

--- python/exercism/python/allergies/allergies.py
+++ python/exercism/python/allergies/allergies.py
@@ -1,7 +1,7 @@
 class Allergies:
 
     def __init__(self, score):
-        self.score = score
+        self.score = score % 256
 
     def allergic_to(self, item):
         return True if item in self.lst else False
@@ -18,8 +18,28 @@ class Allergies:
             64: "pollen",
             128: "cats"
         }
+        allergies = []
         if self.score == 0:
-            allergies = []
+            return allergies
+        if self.score in items.keys():
+            return [items[self.score]]
         else:
-            allergies = [allergy for key, allergy in items.items() if key <= (self.score % 128)]
+            al_codes = [key for key in items.keys() if key <= self.score]
+            score = self.score
+
+            code = al_codes.pop()  # last element is always an allergy
+            allergies.append(items[code])  # append last element
+            score -= code  # subtract from score
+            while True:
+                if score in items.keys():
+                    allergies.append(items[score])
+                    break
+                code = al_codes.pop()
+                allergies.append(items[code])
+                score -= code
+
         return allergies
+
+
+if __name__ == '__main__':
+    print(Allergies(255).lst)
