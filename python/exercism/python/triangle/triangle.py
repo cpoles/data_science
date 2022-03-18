1d2f9b95ad1d5973f0f48860c5a3c5eb033a9ccb
cpoles <cadupoles@gmail.com>
2022-03-18 00:01:51 -0300

    finished triangle

--- python/exercism/python/triangle/triangle.py
+++ python/exercism/python/triangle/triangle.py
@@ -1,10 +1,31 @@
 def equilateral(sides):
-    pass
+    s1, s2, s3 = sides
+    if validate(sides):
+        return True if s1 == s2 and \
+                       s2 == s3 else False
+    return False
 
 
 def isosceles(sides):
-    pass
+    s1, s2, s3 = sides
+    if validate(sides):
+        return True if s1 == s2 or \
+                       s2 == s3 or \
+                       s1 == s3 else False
+    return False
 
 
 def scalene(sides):
-    pass
+    s1, s2, s3 = sides
+    if validate(sides):
+        return True if s1 != s2 and \
+                       s2 != s3 else False
+    return False
+
+
+def validate(sides):
+    s1, s2, s3 = sides
+    if all(map(lambda x: x > 0, sides)):
+        if s1 + s2 >= s3 and s2 + s3 >= s1 and s1 + s3 >= s2:
+            return True
+    return False
