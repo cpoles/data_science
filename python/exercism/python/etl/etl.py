176b2e5076432b8caae770cac18ad901a46db539
cpoles <cadupoles@gmail.com>
2022-05-27 17:05:16 -0300

    finished etl

--- python/exercism/python/etl/etl.py
+++ python/exercism/python/etl/etl.py
@@ -1,2 +1,8 @@
 def transform(legacy_data):
-    pass
+    dics = [{}.fromkeys(v, k) for k, v in legacy_data.items()]
+    data = {}
+    for dic in dics:
+        data.update(dic)
+
+    data = dict(sorted(data.items()))
+    return {k.lower(): v for k, v in data.items()}
