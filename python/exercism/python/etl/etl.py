b7b6dbebe8498bf3d2f5506a5e84be06677703b1
cpoles <cadupoles@gmail.com>
2022-05-27 17:08:50 -0300

    improved etl

--- python/exercism/python/etl/etl.py
+++ python/exercism/python/etl/etl.py
@@ -1,8 +1,2 @@
 def transform(legacy_data):
-    dics = [{}.fromkeys(v, k) for k, v in legacy_data.items()]
-    data = {}
-    for dic in dics:
-        data.update(dic)
-
-    data = dict(sorted(data.items()))
-    return {k.lower(): v for k, v in data.items()}
+    return {letter.lower(): key for key, value in legacy_data.items() for letter in value}
