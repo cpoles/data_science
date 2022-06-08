79ec060d7d49713067b43d1351bf429db0300c07
cpoles <cadupoles@gmail.com>
2022-06-08 18:33:18 -0300

    improved gigasecond

--- python/exercism/python/gigasecond/gigasecond.py
+++ python/exercism/python/gigasecond/gigasecond.py
@@ -1,8 +1,8 @@
-from datetime import timedelta, datetime
+from datetime import timedelta
 
 
 def add(moment):
-    days, seconds = divmod(1e9, 3600*24)
-    hours, seconds = divmod(seconds, 3600)
+    return moment + timedelta(seconds=1e9)
 
-    return moment + timedelta(days=days, hours=hours, seconds=seconds)
+
+
