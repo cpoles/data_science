28cffbad443e775250571b74fb96e0544b7f46de
cpoles <cadupoles@gmail.com>
2022-06-08 21:27:50 -0300

    finished secret_handshake

--- python/exercism/python/secret-handshake/secret_handshake.py
+++ python/exercism/python/secret-handshake/secret_handshake.py
@@ -1,2 +1,10 @@
 def commands(binary_str):
-    pass
+    handshakes = ["reverse", "jump", "close your eyes", "double blink", "wink"]
+    bins = [int(digit) for digit in binary_str]
+
+    result = [handshake for n, handshake in zip(bins, handshakes) if n == 1]
+
+    if 'reverse' in result:
+        return result[1:]
+    else:
+        return result[::-1]
