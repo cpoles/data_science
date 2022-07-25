31c2cd74eed6fd1017c9386dd5deffed623a7197
cpoles <cadupoles@gmail.com>
2022-07-25 16:12:49 -0300

    finished flatten array

--- python/exercism/python/flatten-array/flatten_array.py
+++ python/exercism/python/flatten-array/flatten_array.py
@@ -1,2 +1,13 @@
-def flatten(iterable):
-    pass
+def flatten(list_of_lists):
+    # base case
+    if not list_of_lists:
+        return list_of_lists
+
+    head, *tail = list_of_lists
+
+    if head is None:
+        return flatten(tail)
+    else:
+        if isinstance(head, list):
+            return flatten(head) + flatten(tail)
+        return [head] + flatten(tail)
