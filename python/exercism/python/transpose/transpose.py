10bdb7a2ff1b160742356c7c08d86ae2910cb7cf
cpoles <cadupoles@gmail.com>
2022-08-19 17:05:18 -0300

    implemented transpose method

--- python/exercism/python/transpose/transpose.py
+++ python/exercism/python/transpose/transpose.py
@@ -1,2 +1,18 @@
 def transpose(lines):
-    pass
+    if not lines:
+        return ''
+
+    lines = lines.split('\n')
+    # get the longest string in the list
+    max_len = max([len(line) for line in lines])
+    min_len = min([len(line) for line in lines])
+    print(min_len)
+    # pad all strings with spaces to match same size
+    lines = [line.ljust(max_len) for line in lines]
+    # zip all elements of the list
+    lines = list(map(lambda x: ''.join(x), zip(*lines)))
+    lines[min_len:] = list(map(lambda x: x.rstrip(), lines[min_len:]))
+
+    return '\n'.join(lines)
+
+
