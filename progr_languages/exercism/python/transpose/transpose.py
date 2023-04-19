0ad79a0c935c091515af67586e44e6f35846c986
cpoles <cadupoles@gmail.com>
2023-04-19 22:28:31 +1000

    one line solution

--- progr_languages/exercism/python/transpose/transpose.py
+++ progr_languages/exercism/python/transpose/transpose.py
@@ -1,35 +1,6 @@
+from itertools import zip_longest
+
 def transpose(lines):
-    if not lines:
-        return "\n".join(lines)
-
-    lines = lines.split('\n')
-    # get the longest string in the list
-    max_len = max([len(line) for line in lines])
-    min_len = min([len(line) for line in lines])
-    print(min_len)
-    # pad all strings with spaces to match same size
-    lines = [line.ljust(max_len) for line in lines]
-    # zip all elements of the list
-    lines = list(map(lambda x: ''.join(x), zip(*lines)))
-
-    # length of the largest line containing the penultimate period at the end of the line
-    mlen = 0
+    lines = zip_longest(*lines.splitlines(), fillvalue='@')
     
-    for idx, line  in enumerate(lines):
-        if line[-1] == '.':
-            mlen = len(line)
-            for i in range(idx+1, len(lines)):
-                lines[i] = lines[i][:-1]
-        
-        if line == lines[-1]:
-            lines[-1] = line.rstrip()
-
-    for idx, line in enumerate(lines):
-        if all([c == " " for c in line]):
-            continue
-
-        if len(line) > len(lines[-1]) and len(line) < mlen:
-            lines[idx] = line.rstrip()
-
-
-    return "\n".join(lines)
+    return '\n'.join(''.join(line).rstrip('@').replace('@', ' ') for line in lines)
