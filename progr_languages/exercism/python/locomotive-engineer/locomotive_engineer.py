990a04018b06010c289921b80e0c825d6389c50f
cpoles <cadupoles@gmail.com>
2023-04-20 20:54:50 +1000

    finished locomotive-engineer

--- progr_languages/exercism/python/locomotive-engineer/locomotive_engineer.py
+++ progr_languages/exercism/python/locomotive-engineer/locomotive_engineer.py
@@ -1,14 +1,13 @@
 """Functions which helps the locomotive engineer to keep track of the train."""
 
 
-def get_list_of_wagons():
+def get_list_of_wagons(*args):
     """Return a list of wagons.
 
     :param: arbitrary number of wagons.
     :return: list - list of wagons.
     """
-    pass
-
+    return list(args)
 
 def fix_list_of_wagons(each_wagons_id, missing_wagons):
     """Fix the list of wagons.
@@ -17,17 +16,18 @@ def fix_list_of_wagons(each_wagons_id, missing_wagons):
     :parm missing_wagons: list - the list of missing wagons.
     :return: list - list of wagons.
     """
-    pass
+    first, second, third, *rest = each_wagons_id
+    return [third, *missing_wagons, *rest, first, second]
 
-
-def add_missing_stops():
+def add_missing_stops(route, **stops):
     """Add missing stops to route dict.
 
     :param route: dict - the dict of routing information.
     :param: arbitrary number of stops.
     :return: dict - updated route dictionary.
     """
-    pass
+    stops_merged = {"stops": list(stops.values())}
+    return {**route, **stops_merged}
 
 
 def extend_route_information(route, more_route_information):
@@ -37,8 +37,7 @@ def extend_route_information(route, more_route_information):
     :param more_route_information: dict -  extra route information.
     :return: dict - extended route information.
     """
-    pass
-
+    return {**route, **more_route_information}
 
 def fix_wagon_depot(wagons_rows):
     """Fix the list of rows of wagons.
@@ -46,4 +45,12 @@ def fix_wagon_depot(wagons_rows):
     :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
     :return: list[list[tuple]] - list of rows of wagons.
     """
-    pass
+    firsts, seconds, thirds = [], [], []
+
+    for wagon in wagons_rows:
+        f, s, t = wagon
+        firsts.append(f)
+        seconds.append(s)
+        thirds.append(t)
+    return [firsts, seconds, thirds]
+
