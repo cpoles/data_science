887589164d46f74225677f6f0b1b646442de6532
cpoles <cadupoles@gmail.com>
2023-04-27 14:21:09 +1000

    added helpers module

--- projects/streamlit_classification/app.py
+++ projects/streamlit_classification/app.py
@@ -7,14 +7,7 @@ import time
 import numpy as np
 import pandas as pd
 import streamlit as st
-import sklearn.metrics
-import sklearn.datasets
-import ydata_profiling
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-from streamlit_pandas_profiling import st_profile_report
+from helpers import hp
 
 # Title bar
 st.title(f"ML Classification with Logistic Regression")
@@ -40,85 +33,4 @@ max_iter = st.sidebar.text_input('Number of iterations: (default = 50)', '50')
 params = {'penalty': penalty, 'tol': float(tol), 'max_iter': int(max_iter), 'solver': solver}
 
 
-# --- HELPER FUNCTIONS --- #
-@st.cache_data
-def load_data(dataset):
-    '''
-        Loads a sklearn dataset
-
-        :param str dataset: The name of the dataset to be loaded
-
-        :return: sklearn dataset
-    '''
-    if dataset == 'Iris':
-        data = sklearn.datasets.load_iris()
-    elif dataset == 'Wine':
-        data = sklearn.datasets.load_wine()
-    else:
-        data = sklearn.datasets.load_breast_cancer()
-
-    return data
-
-def data_preprocess(dataset, test_size):
-    '''
-        Preprocesses the dataset for training
-
-        :param np.array dataset: The whole dataset
-        :param float test_size
-
-        :return: tuple with train and test data
-    '''
-    # Split dataset into train and test
-    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=float(test_size), random_state=42)
-
-    # Standardize data
-    scaler = MinMaxScaler()
-
-    # Fit and transform train data
-    X_train = scaler.fit_transform(X_train)
-    
-    # Transform test data
-    X_test = scaler.transform(X_test)
-
-    return X_train, X_test, y_train, y_test
-
-def create_model(params, train_data):
-    '''
-        Returns a trained Logistic Regression model
-
-        :param dict params: Dictionary of hyperparameters
-        :param tuple train_data: The training data (X_train, y_train)
-
-        :return  Trained LR model
-    '''
-
-    # Load train data
-    X_train, y_train = train_data
-
-    # Create LR Clf Model
-    clf = LogisticRegression(solver=params['solver'],
-                             max_iter=params['max_iter'],
-                             penalty=params['penalty'],
-                             tol=params['tol']) 
-    
-    # Fit the Model
-    clf.fit(X_train, y_train)
-
-    return clf
-
-def create_profile_report(dataset):
-    '''
-        Create a profile report of a given dataset
-
-        :param np.array dataset: The dataset to be profiled
-
-        :return profile report of dataset
-    '''
-    # concatenate target and features
-    data = np.hstack([dataset.data, dataset.target.reshape(dataset.data.shape[0], -1)])
-    # create dataframe
-    df = pd.DataFrame(data=data, columns=dataset.feature_names+['target'])
-    # create profile report
-    pr = df.profile_report()
-    return pr, df
 
---
+++ projects/streamlit_classification/helpers/hp.py
@@ -0,0 +1,115 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import sklearn.datasets
+import ydata_profiling
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+from streamlit_pandas_profiling import st_profile_report
+
+
+# --- HELPER FUNCTIONS --- #
+@st.cache_data
+def load_data(dataset):
+    '''
+        Loads a sklearn dataset
+
+        :param str dataset: The name of the dataset to be loaded
+
+        :return: sklearn dataset
+    '''
+    if dataset == 'Iris':
+        data = sklearn.datasets.load_iris()
+    elif dataset == 'Wine':
+        data = sklearn.datasets.load_wine()
+    else:
+        data = sklearn.datasets.load_breast_cancer()
+
+    return data
+
+def data_preprocess(dataset, test_size):
+    '''
+        Preprocesses the dataset for training
+
+        :param np.array dataset: The whole dataset
+        :param float test_size
+
+        :return: tuple with train and test data
+    '''
+    # Split dataset into train and test
+    X_train, X_test, y_train, y_test = train_test_split(dataset.data, dataset.target, test_size=float(test_size), random_state=42)
+
+    # Standardize data
+    scaler = MinMaxScaler()
+
+    # Fit and transform train data
+    X_train = scaler.fit_transform(X_train)
+    
+    # Transform test data
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
+
+def create_model(params, train_data):
+    '''
+        Returns a trained Logistic Regression model
+
+        :param dict params: Dictionary of hyperparameters
+        :param tuple train_data: The training data (X_train, y_train)
+
+        :return  Trained LR model
+    '''
+
+    # Load train data
+    X_train, y_train = train_data
+
+    # Create LR Clf Model
+    clf = LogisticRegression(solver=params['solver'],
+                             max_iter=params['max_iter'],
+                             penalty=params['penalty'],
+                             tol=params['tol']) 
+    
+    # Fit the Model
+    clf.fit(X_train, y_train)
+
+    return clf
+
+def create_profile_report(dataset):
+    '''
+        Create a profile report of a given dataset
+
+        :param np.array dataset: The dataset to be profiled
+
+        :return profile report of dataset
+    '''
+    # concatenate target and features
+    data = np.hstack([dataset.data, dataset.target.reshape(dataset.data.shape[0], -1)])
+    # create dataframe
+    df = pd.DataFrame(data=data, columns=dataset.feature_names+['target'])
+    # create profile report
+    pr = df.profile_report()
+    return pr, df
+
+def get_model_metrics(model, test_set):
+    '''
+        Returns model predictions, confusion matrix and accuracy
+
+        :params model: sklearn LR model
+        :params sklearn.linear_model._logistic.LogisticRegression
+    
+        :params np.array test_set: The dataset used for predictions
+
+        :return dict predictions, accuracy, confusion matrix
+    '''
+    # Unpack test set
+    X_test, y_test = test_set
+    # Predictions
+    predictions = model.predict(X_test)
+    # Accuracy
+    accuracy = accuracy_score(y_test, predictions)
+    # Confusion Matrix
+    c_matrix = confusion_matrix(y_test, predictions)
+
+    return predictions, accuracy, c_matrix
