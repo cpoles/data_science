b02288183a447df401ea3ad47a998cce0d02cf86
cpoles <cadupoles@gmail.com>
2023-12-24 10:56:50 +1000

    removed todos

--- progr_languages/exercism/cpp/lasagna/lasagna.cpp
+++ progr_languages/exercism/cpp/lasagna/lasagna.cpp
@@ -6,7 +6,6 @@ const int TIME_PER_LAYER = 2;
 // ovenTime returns the amount in minutes that the lasagna should stay in the
 // oven.
 int ovenTime() {
-    // TODO: Return the correct time.
     return COOKING_TIME;
 }
 
@@ -14,7 +13,6 @@ int ovenTime() {
    minutes based on the actual minutes already in the oven.
 */
 int remainingOvenTime(int actualMinutesInOven) {
-    // TODO: Calculate and return the remaining in the oven based on the time
     // the lasagna has already been there.
     return ovenTime() - actualMinutesInOven;
 }
@@ -23,14 +21,11 @@ int remainingOvenTime(int actualMinutesInOven) {
    number of layers and the necessary time per layer.
 */
 int preparationTime(int numberOfLayers) {
-    // TODO: Calculate and return the preparation time with the
-    // `numberOfLayers`.
     return TIME_PER_LAYER * numberOfLayers;
 }
 
 // elapsedTime calculates the total time spent to create and bake the lasagna so
 // far.
 int elapsedTime(int numberOfLayers, int actualMinutesInOven) {
-    // TODO: Calculate and return the total time so far.
     return preparationTime(numberOfLayers) + actualMinutesInOven;
 }
