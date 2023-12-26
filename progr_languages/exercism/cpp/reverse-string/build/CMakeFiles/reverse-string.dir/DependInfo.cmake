eda90699fa65b023458d364c43e3e5009dcb59bf
cpoles <cadupoles@gmail.com>
2023-12-26 22:38:29 +1000

    finished reverse_string

---
+++ progr_languages/exercism/cpp/reverse-string/build/CMakeFiles/reverse-string.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/krusty/Programming/data_science/progr_languages/exercism/cpp/reverse-string/reverse_string.cpp" "CMakeFiles/reverse-string.dir/reverse_string.cpp.o" "gcc" "CMakeFiles/reverse-string.dir/reverse_string.cpp.o.d"
+  "/home/krusty/Programming/data_science/progr_languages/exercism/cpp/reverse-string/reverse_string_test.cpp" "CMakeFiles/reverse-string.dir/reverse_string_test.cpp.o" "gcc" "CMakeFiles/reverse-string.dir/reverse_string_test.cpp.o.d"
+  "/home/krusty/Programming/data_science/progr_languages/exercism/cpp/reverse-string/test/tests-main.cpp" "CMakeFiles/reverse-string.dir/test/tests-main.cpp.o" "gcc" "CMakeFiles/reverse-string.dir/test/tests-main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ progr_languages/exercism/cpp/reverse-string/build/CMakeFiles/reverse-string.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverse-string.dir/reverse_string.cpp.o"
+  "CMakeFiles/reverse-string.dir/reverse_string.cpp.o.d"
+  "CMakeFiles/reverse-string.dir/reverse_string_test.cpp.o"
+  "CMakeFiles/reverse-string.dir/reverse_string_test.cpp.o.d"
+  "CMakeFiles/reverse-string.dir/test/tests-main.cpp.o"
+  "CMakeFiles/reverse-string.dir/test/tests-main.cpp.o.d"
+  "reverse-string"
+  "reverse-string.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reverse-string.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
