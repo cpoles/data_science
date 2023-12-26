2467ccc21a7357d16d73c1e0d0dd4b2770d7c616
cpoles <cadupoles@gmail.com>
2023-12-26 22:46:33 +1000

    updated reverse_string

--- progr_languages/exercism/cpp/reverse-string/reverse_string.cpp
+++ progr_languages/exercism/cpp/reverse-string/reverse_string.cpp
@@ -2,18 +2,7 @@
 
 namespace reverse_string {
 
-    std::string reverse_string(std::string str) {
-        if (str.empty())
-            return "";
-        
-        std::string rev_str;
-        for (size_t i = str.length(); i > 0; i--)
-        {
-            size_t j = str.length() - i;
-            rev_str[j] = str[i];
-        }
-        return rev_str;
-        
-    }
-
+   std::string reverse_string(const std::string& str) {
+        return std::string(str.rbegin(), str.rend());
+   }
 }  // namespace reverse_string
